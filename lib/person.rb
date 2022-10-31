ede8312f3e04f8c8d373d9d2f0fb15dd5d0f49f8
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-31 09:47:29 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Person
-  # your code here
+@@all = []
+  def initialize attributes
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+      @@all << self
+    end
+  end
+  def self.all
+    @@all
+  end
 end
+
+person_1 = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+person_2 = { name: "Bob", hair_color: "Brown"}
+person_3 = {name: "Spencer", hair_color: "N/A", height: "medium", weight: "good", handed: "righty"}
+# binding.pry
+# 0
